0a5d97d6cd7807218455a7f043bdcf71ddcf8442
Nero Leung <nerolileung@gmail.com>
2020-12-15 18:04:33 +0000

    vertical prototype

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private LevelManager _levelManager;
+    
+    public enum CONTROL_SCHEME {
+        SWIPE, TOUCH, BUTTONS
+    }
+    private CONTROL_SCHEME currentControls;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentControls = CONTROL_SCHEME.BUTTONS;
+        DontDestroyOnLoad(this.gameObject);
+        // temp
+        LoadLevel(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LoadLevel(int levelIndex){
+        // load scene
+        SceneManager.LoadScene("Prototype");
+        SceneManager.LoadScene("HUD", LoadSceneMode.Additive);
+    }
+
+    public CONTROL_SCHEME GetControls(){
+        return currentControls;
+    }
+}
---
+++ Assets/Scripts/LevelInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.Audio;
+
+[CreateAssetMenu(fileName = "Data", menuName = "LevelInfo", order = 1)]
+public class LevelInfo : ScriptableObject
+{
+    public string title;
+    //public Sprite image;
+    public float startSpeed;
+    public float endSpeed;
+    // length in units/100px
+    public int levelLength;
+}
